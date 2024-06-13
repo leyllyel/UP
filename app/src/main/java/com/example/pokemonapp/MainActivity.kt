fa057fb7fe3d92956276208a10103393fbc0dea0
sad.ley@icloud.com <sad.ley@icloud.com>
2024-06-13 18:10:44 +0300

    First

---
+++ app/src/main/java/com/example/pokemonapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.pokemonapp
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
+import com.example.pokemonapp.Screen.ScreenActivity
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+       supportFragmentManager.commit {
+           add<ScreenActivity>(R.id.host_fragment)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pokemonapp/Screen/ScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pokemonapp.Screen
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.pokemonapp.R
+
+class ScreenActivity : Fragment(R.layout.activity_screen) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        }
+    }
+
