aebf87734a3a3ff0f197ee1d1ea6c3e7054a316c
sad.ley@icloud.com <sad.ley@icloud.com>
2024-06-14 18:55:06 +0300

    Second Commit

--- app/src/main/java/com/example/pokemonapp/Screen/ScreenActivity.kt
+++ app/src/main/java/com/example/pokemonapp/Screen/ScreenActivity.kt
@@ -1,13 +1,23 @@
 package com.example.pokemonapp.Screen
 
 import android.os.Bundle
+import android.view.MenuInflater
 import android.view.View
+import android.widget.ImageButton
+import androidx.appcompat.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import com.example.pokemonapp.R
 
 class ScreenActivity : Fragment(R.layout.activity_screen) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val changeModeButton = view.findViewById<ImageButton>(R.id.ChangeMode)
+        changeModeButton.setOnClickListener {
+            val menu = PopupMenu(requireContext(), it)
+            val inflater: MenuInflater= menu.menuInflater
+            inflater.inflate(R.menu.search_menu,menu.menu,)
+            menu.show()
+        }
 
         }
     }
